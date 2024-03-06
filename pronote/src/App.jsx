2e41b733cccf53070f3d79f3e7d7368ecd3e65c2
harsha-1807 <harshavardhan.sr@kalvium.community>
2024-03-06 11:36:37 +0530

    deployment completed

---
+++ pronote/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Edit from './components/Editor';
+
+function App() {
+  return (
+    <div className="App">
+      <Edit/>
+    </div>
+  );
+}
+
+export default App;
